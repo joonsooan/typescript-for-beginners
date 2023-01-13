0901bbdd33928f462fbe31ff944f2b16300dd56f
joonsooan <joonsooan@gmail.com>
2023-01-13 21:41:17 +0900

    Update hello.ts

--- hello.ts
+++ hello.ts
@@ -2,6 +2,20 @@ let a: number = 123;
 let b: string = "hello";
 let c: string[] = ["a", "b", "c", "d"];
 
-const person = {
-  name: "Jason",
+type Player = {
+  name: string;
+  age?: number;
 };
+
+const Bryan: Player = {
+  name: "Bryan",
+};
+
+const Bob: Player = {
+  name: "Bob",
+};
+
+const playerMaker = (name: string): Player => ({ name });
+
+const jason = playerMaker("jason");
+jason.age = 12;
